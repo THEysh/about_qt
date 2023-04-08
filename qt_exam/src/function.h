2af8de8e833cfbb82945c78671a16d31ef21f1af
THEysh <1084701312@QQ.COM>
2023-04-08 18:08:14 +0800

    1

--- qt_exam/src/function.h
+++ qt_exam/src/function.h
@@ -16,7 +16,8 @@
 #include <QSignalMapper>
 #include <QString>
 #include <QPushButton>
-#include "myfirst.h"
+#include "ui/myfirst_ui.h"
+#include "ui/Qstring_Interface_switching_ui.h"
 
 using namespace  std;
 
@@ -24,10 +25,13 @@ class ThreadReceiver : public QObject
 {
     /**
      * /这个类用于多线程信号通信，do_work_Something函数绘睡眠，睡眠完成后会发送信号“work is done”,主函数可以参考如下：
+     * main:
+    ThreadReceiver receiver(ui_f); double sleep_time = 1; long dim =192 ;
+    receiver.button1_connect(receiver.ui_f.pushButton,sleep_time);
+    receiver.button2_connect(receiver.ui_f.pushButton_2,sleep_time, dim); //将dim10进制转为2进制(10亿以内)
      */
 Q_OBJECT
-public:
-    Ui_MainWindow ui_f{};
+
 signals:
     void signal_1(QWidget *qw, QString q_ans);
     //定义一个信号，并定义与这个信号连接的槽函数 void onworkFinished
@@ -39,11 +43,15 @@ public slots:
     void do_work_Something(QWidget *qw, double sleep_time); //多线程睡眠，计算随机数的一个乘法
     void do_work_decimal_to_binary(QWidget *qw,double sleep_time,long number); //多线程睡眠，计算十进制转二进制
 public:
-    ThreadReceiver(Ui_MainWindow ui_f){
+    Ui_MainWindow ui_f;
+    double sleep_time = 0.5;
+    ThreadReceiver(Ui_MainWindow &ui_f, QObject* parent = nullptr) : QObject(parent){
         // 创建信号的连接
         this->ui_f = ui_f;
         QObject::connect(this, &ThreadReceiver::signal_1, this, &ThreadReceiver::onworkFinished);
 
+        this->button1_connect(ui_f.pushButton, sleep_time);
+        this->button2_connect(ui_f.pushButton_2, sleep_time,12345);
     }
 
     void button1_connect(QPushButton *button, double sleep_time);
@@ -54,5 +62,123 @@ public:
 
 };
 
+class Interface_Class : public QObject{
+public:
+    Ui_Interface_MainWindow  ui_f;
+    Interface_Class(Ui_Interface_MainWindow  &ui_f){
+        this->ui_f = ui_f;
+        this->connect_but1();
+        this->connect_but2();
+        this->connect_but2_2();
+        this->connect_but3();
+        this->connect_but4();
+        this->connect_but5();
+        this->connect_but6();
+        this->connect_but7();
+        this->connect_but8();
+        this->connect_but9();
+        this->connect_but10();
+    }
+    void connect_but1 (){
+        QObject::connect(ui_f.pushButton_1, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            QString str3 = ui_f.lineEdit3->text();
+            ui_f.lineEdit3->setText(str3.append(str1).append(str2));
+        });
+    }
+    void connect_but2 (){
+        QObject::connect(ui_f.pushButton_2_1, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            QString str3 = ui_f.lineEdit3->text();
+            ui_f.lineEdit3->setText(str3.prepend(str1).prepend(str2));
+        });
+    }
+    void connect_but2_2(){
+        QObject::connect(ui_f.pushButton_2_2, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            ui_f.lineEdit3->clear();
+            QString re_text = QString("str1的长度计算：%1, str2的长度计算%2").arg(str1.size()).arg(str2.size());
+            ui_f.lineEdit3->setText(re_text);
+        });
+    }
+    void connect_but3(){
+        QObject::connect(ui_f.pushButton_3, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            ui_f.lineEdit3->clear();
+            QString re_text = QString("str1的转换大写计算:%1, str2的长度转换大写:%2").arg(str1.toUpper()).arg(str2.toUpper());
+            ui_f.lineEdit3->setText(re_text);
+        });
 
+    }
+
+    void connect_but4(){
+        QObject::connect(ui_f.pushButton_4, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            ui_f.lineEdit3->clear();
+            QString re_text = QString("str1的转换大写计算:%1, str2的长度转换大写:%2").arg(str1.toLower()).arg(str2.toLower());
+            ui_f.lineEdit3->setText(re_text);
+        });
+    }
+    void connect_but5 (){
+        connect(ui_f.pushButton_5, &QPushButton::clicked,[=]{
+            ui_f.stackedWidget->setCurrentIndex(0);
+        });
+    }
+    void connect_but6 (){
+        QObject::connect(ui_f.pushButton_6, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            ui_f.lineEdit3->clear();
+            QString re_text;
+            if(str1.isEmpty() || str2.isEmpty()){
+                re_text = "str1中不存在str2";
+            }
+            else if (str1.contains(str2)){
+                re_text = "str1中存在str2";
+            }
+            else{
+                re_text = "str1中不存在str2";
+            }
+            ui_f.lineEdit3->setText(re_text);
+        });
+    }
+
+    void connect_but7 (){
+        QObject::connect(ui_f.pushButton_7, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            ui_f.lineEdit3->clear();
+            QString re_text = QString("str1:%1,str2:%2").arg(str1.isNull()).arg(str2.isNull());
+            ui_f.lineEdit3->setText(re_text);
+        });
+    }
+    void connect_but8 (){
+        QObject::connect(ui_f.pushButton_7, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            ui_f.lineEdit3->clear();
+            QString re_text = QString("str1:%1,str2:%2").arg(str1.isEmpty()).arg(str2.isEmpty());
+            ui_f.lineEdit3->setText(re_text);
+        });
+    }
+    void connect_but9(){
+        QObject::connect(ui_f.pushButton_9, &QPushButton::clicked,[&](){
+            QString str1 = ui_f.lineEdit1->text();
+            QString str2 = ui_f.lineEdit2->text();
+            ui_f.lineEdit3->clear();
+            QString re_text = QString("str1是不是以str2开头(1表示true，0表示false):%1").arg(str1.startsWith(str2));
+            ui_f.lineEdit3->setText(re_text);
+        });
+    }
+    void connect_but10 (){
+        connect(ui_f.pushButton_10, &QPushButton::clicked,[=]{
+            ui_f.stackedWidget->setCurrentIndex(1);
+        });
+    }
+};
 #endif //QT_EXAM_FUNCTION_H
--- qt_exam/src/main.cpp
+++ qt_exam/src/main.cpp
@@ -3,23 +3,28 @@
 #include <QHBoxLayout>
 #include <QLabel>
 #include "iostream"
-#include "myfirst.h"
+#include "ui/myfirst_ui.h"
+#include "ui/Qstring_Interface_switching_ui.h"
 #include "function.h"
 
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     auto MainWindow = new QMainWindow();
     Ui_MainWindow ui_f{};
     ui_f.setupUi(MainWindow);
-    //创建一个信号接受者，用于接收信号（这个类里面有槽函数）
-    ThreadReceiver receiver(ui_f); double sleep_time = 1; long dim =  123456789;
-
-    receiver.button1_connect(receiver.ui_f.pushButton,sleep_time);
-    receiver.button2_connect(receiver.ui_f.pushButton_2,sleep_time, dim); //将dim10进制转为2进制(10亿以内)
+    ThreadReceiver receiver_main(ui_f);
     MainWindow->show();
 
+    auto MainWindow2 = new QMainWindow();
+    Ui_Interface_MainWindow ui_interface{};
+    ui_interface.setupUi(MainWindow2);
+    Interface_Class ui_interface_main(ui_interface);
+    MainWindow2->show();
+
+
+
     return QApplication::exec();
+
 }
